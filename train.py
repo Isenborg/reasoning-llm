f21f877ca2a4f1d5f29a885e92813ed1fd8b31aa
Isenborg <misenborg@hotmail.com>
2026-03-08 23:57:10 +0100

    around 700 examples/hour

--- train.py
+++ train.py
@@ -3,12 +3,22 @@ from training.grpo import GRPOTrainer
 from grpo import utils
 from utils import models
 
-model_name = "Qwen/Qwen3-1.7B-Base-sft"
+model_name = "grpo_model-best"
 
 model = models.load_model(model_name)
 tokenizer = models.load_tokenizer(model_name)
 
-config = GRPOConfig(G=6, max_steps=200, batch_size=2, use_kl=False, eval_samples=50, eval_every=50, K=1, lr=2e-6, max_new_tokens=512)
+config = GRPOConfig(G=8, 
+                    max_steps=200, 
+                    batch_size=4, 
+                    use_kl=False, 
+                    eval_samples=16, 
+                    eval_every=50, 
+                    K=1, 
+                    lr=5e-6, 
+                    max_new_tokens=512, 
+                    use_8bit_optim=True)
+
 utils.estimate_vram(model, config)
 
 
@@ -20,5 +30,5 @@ train_dataset = GSM8KDataset(split="train")   # ~7.5k examples
 # Evaluation
 test_dataset = GSM8KDataset(split="test")      # ~1.3k examples
 
-trainer = GRPOTrainer(model, None, tokenizer, rewards.calculate_reward, config)
-trainer.train(train_dataset, test_dataset)
+trainer = GRPOTrainer(model, tokenizer, rewards.calculate_reward, config)
+trainer.train(train_dataset, test_dataset)  
